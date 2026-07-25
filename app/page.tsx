d53fdc95302fb58302a392c53432479a0358f037
Aman <amansinha561@gmail.com>
2026-07-25 23:32:32 +0530

    SSR fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 const Home = () => {
     return(
         <div>
-            <div className="h-screen flex items-center justify-center">
+            <div className="h-screen flex items-center justify-center flex-col">
                 <p className="text-6xl font-bold">Welcome to Anonly</p>
+                <p>Send your secret message anonymously</p>
+                <div className="my-2">
+                    <button className="bg-[#dddddd] p-4 dark:bg-[#121212] rounded-4xl font-bold">
+                        Make for me
+                    </button>
+                </div>
             </div>
         </div>
     )
--- components/ThemeChanger.tsx
+++ components/ThemeChanger.tsx
@@ -1,13 +1,30 @@
-"use client"
-import { useTheme } from "next-themes";
-import { Sun, Moon } from "lucide-react";
+"use client";
 
-export const ThemeChanger = () =>{
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export function ThemeChanger() {
   const { resolvedTheme, setTheme } = useTheme();
- 
-    return (
-        <div>
-            <button onClick={() => resolvedTheme == 'light' ? setTheme('dark') : setTheme('light')}>{resolvedTheme == 'light' ? <Moon size={24} /> : <Sun size={24} />}</button>
-        </div>
-    )
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <button
+      onClick={() =>
+        setTheme(resolvedTheme === "light" ? "dark" : "light")
+      }
+    >
+      {resolvedTheme === "light" ? (
+        <Moon size={24} />
+      ) : (
+        <Sun size={24} />
+      )}
+    </button>
+  );
 }
