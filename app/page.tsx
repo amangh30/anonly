df825615194aaf0ee464fe9dd3eaf433f9f39d6e
Aman <amansinha561@gmail.com>
2026-07-25 23:45:10 +0530

    Add Form

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
+import Link from "next/link";
+
 const Home = () => {
     return(
         <div>
             <div className="h-screen flex items-center justify-center flex-col">
                 <p className="text-6xl font-bold">Welcome to Anonly</p>
                 <p>Send your secret message anonymously</p>
-                <div className="my-2">
-                    <button className="bg-[#dddddd] p-4 dark:bg-[#121212] rounded-4xl font-bold">
+                <div className="my-5">
+                    <Link href='/user-details' className="bg-[#dddddd] p-4 dark:bg-[#121212] rounded-4xl font-bold">
                         Make for me
-                    </button>
+                    </Link>
                 </div>
             </div>
         </div>
---
+++ app/user-details/page.tsx
@@ -0,0 +1,19 @@
+const Page = ()=>{
+    return(
+        <div className="h-full w-full">
+            <div className="flex flex-col items-center justify-center my-5">
+                <p>
+                    Just one thing before we get those texts
+                </p>
+                <p>
+                    Enter your name
+                </p>
+                <input type="text" />
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Page;
