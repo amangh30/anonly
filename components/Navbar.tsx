dbfcea964207c421b85058e684463dae43bfa36d
Aman <amansinha561@gmail.com>
2026-07-29 21:16:15 +0530

    Update Nav Style

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,7 @@ import { ThemeChanger } from "./ThemeChanger";
 
 const Navbar = () => {
     return (
-        <div className="h-10 bg-[#EDEDED] dark:bg-[#121212] rounded-b-2xl">
+        <div className="h-10 rounded-b-2xl border border-white/10 bg-white/10 backdrop-blur-md shadow-lg dark:border-white/5 dark:bg-white/5">
             <div className="flex justify-between p-2">
                 <div>
                     Anonly
