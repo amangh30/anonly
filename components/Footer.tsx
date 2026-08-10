275c4ff3cf38a597dc48820aab82cccad9647a46
Aman <amansinha561@gmail.com>
2026-08-10 23:12:28 +0530

    Add Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () =>{
+    return(
+        <div className="flex align-middle justify-between">
+            <div>
+            Reach out:
+            </div>
+            <div>
+            Instagram
+            </div>
+            <div>
+            Twitter
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
