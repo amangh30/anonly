c80a03a55b179964c1b64a965741b4cc968a6109
Aman <amansinha561@gmail.com>
2026-08-21 23:01:31 +0530

    Update Maker

--- app/user-details/page.tsx
+++ app/user-details/page.tsx
@@ -1,18 +1,14 @@
-const Page = () => {
-  return (
-    <div
-      style={{
-        minHeight: "100vh",
-        width: "100%",
-        backgroundColor: "red",
-        backgroundImage: "url('/herobg-light.png')",
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-      }}
-    >
-      HELLO
-    </div>
-  );
-};
+import React from 'react'
 
-export default Page;
+function Page() {
+  return (
+    <div className="h-screen bg-[url('/herobg-light.png')] dark:bg-[url('/herobg.png')]  bg-cover bg-center bg-no-repeat">
+     <div className='flex h-[50%] justify-around items-center'>
+        <p className='font-black text-5xl'>Hey there Name</p>
+        <p>Messages</p>
+      </div> 
+    </div>
+  )
+}
+
+export default Page
